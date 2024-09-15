4d243a48d03cdf2d1a1e0e4e799776f14e5fb0d2
wuliKunKun <terky1019@163.com>
2024-09-15 15:30:43 +0800

    init without anything

---
+++ src/test/java/com/kunkun/apiplatform/ApiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunkun.apiplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
